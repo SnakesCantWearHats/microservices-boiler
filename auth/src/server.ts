4b931a7fa36fae91819867edc5f1c72cab93ad67
Adomas Skliutas <adomas.skliutas@instarem.com>
2019-09-30 21:14:56 +0300

    Removed tests, because who need them

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -27,10 +27,6 @@ app.get('/ping', (req, res) =>  {
 	res.json({ pong: 'pong' });
 });
 
-app.get('/', (req, res) => {
-	res.json({ service: 'Auth', success: true });
-});
-
 app.use('/api/v1', routes);
 
 app.listen(port, () => console.log(`Authentication service is listening on port ${port}`))
